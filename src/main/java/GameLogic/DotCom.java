3b89899df6c9fdac033f4f866379ba8521cdae55
Татьяна Ширшова < >
2017-03-23 20:00:35 +0300

    Initial commit

---
+++ src/main/java/GameLogic/DotCom.java
@@ -0,0 +1,19 @@
+package GameLogic;
+
+import java.util.List;
+
+/**
+ * Created by User on 23.03.2017.
+ */
+public class DotCom {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocationCells(List<String> locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public String checkYourself(String userGuess) {
+    }
+}
---
+++ src/main/java/GameLogic/GameHelper.java
@@ -0,0 +1,14 @@
+package GameLogic;
+
+import java.util.List;
+
+/**
+ * Created by User on 23.03.2017.
+ */
+public class GameHelper {
+    public List<String> placeDotCom(int i) {
+    }
+
+    public String getUserInput(String s) {
+    }
+}
